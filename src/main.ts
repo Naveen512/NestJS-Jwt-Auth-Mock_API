155beaf0d19fe0dddbcefb63e984e6b683e529ee
hchowdary <hchowdary@qualminds.com>
2021-04-13 15:03:51 +0530

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    allowedHeaders:"*"
+  });
   const options = new DocumentBuilder()
     .setTitle('JWT example')
     .setDescription('The jwt API description')
