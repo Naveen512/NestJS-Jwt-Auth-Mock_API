7b2ab27e7090683973efae327e8eb0a055b8fd7e
hchowdary <hchowdary@qualminds.com>
2022-07-16 15:24:32 +0530

    fav-movie endpoint added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Request, Post, UseGuards,Get } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
-import { AuthGuard } from '@nestjs/passport'
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
 
-  
   @UseGuards(AuthGuard('local'))
   @Post('auth/login')
   async login(@Request() req) {
@@ -15,13 +14,40 @@ export class AppController {
 
   @UseGuards(AuthGuard('jwt'))
   @Get('todos')
-  getTodos(){
+  getTodos() {
     return ['Watch Movie', 'Take Health Test', 'Play Cricket'];
   }
 
   @UseGuards(AuthGuard('jwt-refreshtoken'))
   @Post('auth/refreshtoken')
-  async refreshToken(@Request() req){
+  async refreshToken(@Request() req) {
     return await this.authService.login(req.user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('user/fav-movies')
+  getFavMovies() {
+    return [
+      {
+        id: 1,
+        name: 'Iron Man 1',
+        genre: 'Action & Fiction',
+      },
+      {
+        id: 2,
+        name: 'A Quiet Place',
+        genre: 'Horror',
+      },
+      {
+        id: 3,
+        name: 'Joker',
+        genre: 'Drama'
+      },
+      {
+        id:4,
+        name:'Ted',
+        genre: 'Comedy'
+      }
+    ];
   }
 }
